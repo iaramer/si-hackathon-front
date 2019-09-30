62d735ba877a8fdf7f2dc6bdbda17fb29bd1c9e0
Yuri Lysov <yuri_lysov@epam.com>
2019-10-01 00:09:11 +0300

    First commit
    Init project
    
    Changed .gitignore file
    
    app provider
    
    revert branch
    
    this is work

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+// import { Header } from './components/Header';
+// import { countryContainer } from './containers/countyContainer';
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+      <Route  />
+      <Switch>
+        {/* <Route exact path='/' component={countryContainer} /> */}
+      </Switch>
+    </React.Fragment>
+    );
+  }
+}
+
+export default App;
