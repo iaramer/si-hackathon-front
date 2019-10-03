50d2365baf48757631b879d609c31a26304840b6
Anastasiia_Sokolova <anastasiia_sokolova2@epam.com>
2019-10-03 23:09:09 +0300

    add header ad sass

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
-// import { Header } from './components/Header';
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import { Header } from "./header/header";
 // import { countryContainer } from './containers/countyContainer';
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-      <Route  />
-      <Switch>
-        {/* <Route exact path='/' component={countryContainer} /> */}
-      </Switch>
-    </React.Fragment>
+        <Route component={Header} />
+        <Switch>
+          {/* <Route exact path='/' component={countryContainer} /> */}
+        </Switch>
+      </React.Fragment>
     );
   }
 }
