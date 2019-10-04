80dfb0215f6d488c074172a6155b413a120e7ad9
Anastasiia_Sokolova <anastasiia_sokolova2@epam.com>
2019-10-05 01:01:19 +0300

    connection with back

--- src/containers/requires.jsx
+++ src/containers/requires.jsx
@@ -31,6 +31,8 @@
 // 	);
 // }
 
+const url = "http://10.66.169.253:8080";
+
 export const getTopic = () => {
   return fetch("https://jsonplaceholder.typicode.com/posts", {
     method: "GET"
@@ -40,45 +42,32 @@ export const getTopic = () => {
       return json;
     });
 };
-export const getEveningCourses = () => {
-    return fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response => response.json())
-        .then(result => result)
-        .catch(error => new Error(error))
-}
 
-export const getLectureExams = () => {
-    return fetch('https://jsonplaceholder.typicode.com/posts?&_limit=15')
-        .then(response => response.json())
-        .then(result => result)
-        .catch(error => new Error(error))
-}
+export const getLanguages = examId => {
+  return fetch(`${url}/languages?examId=${examId}`, { method: "GET" })
+    .then(response => response.json())
+    .then(result => result)
+    .catch(error => new Error(error));
+};
 
-export const getAssesment = () => {
-    return fetch('https://jsonplaceholder.typicode.com/users?&_limit=15')
-        .then(response => response.json())
-        .then(result => result)
-        .catch(error => new Error(error))
-}
+// list: function() {
+// 	let { pokemons, page } = this.state;
+// 	fetch(`http://localhost:4000/pokemons?_page=${page}`)
+// 	.then( response => response.json())
+// 	.then( result => this.setState({
+// 		pokemons: pokemons.concat(result),
+// 		page: page + 1
+// 		})
+// 	);
+// },
 
-	// list: function() {
-	// 	let { pokemons, page } = this.state;
-	// 	fetch(`http://localhost:4000/pokemons?_page=${page}`)
-	// 	.then( response => response.json())
-	// 	.then( result => this.setState({
-	// 		pokemons: pokemons.concat(result),
-	// 		page: page + 1
-	// 		})	
-	// 	);
-	// },
-	
-	// caught: function() {
-	// 	let { pokemons, page } = this.state;
-	// 	fetch(`http://localhost:4000/pokemons?q=Caught&_page=${page}`)
-	// 	.then( response => response.json())
-	// 	.then( result => this.setState({
-	// 		pokemons: pokemons.concat(result),
-	// 		page: page + 1
-	// 	})
-	// 	);
-	// }
+// caught: function() {
+// 	let { pokemons, page } = this.state;
+// 	fetch(`http://localhost:4000/pokemons?q=Caught&_page=${page}`)
+// 	.then( response => response.json())
+// 	.then( result => this.setState({
+// 		pokemons: pokemons.concat(result),
+// 		page: page + 1
+// 	})
+// 	);
+// }
--- src/containers/start-page/start-page.component.jsx
+++ src/containers/start-page/start-page.component.jsx
@@ -1,89 +1,76 @@
-import * as React from 'react';
-import { getEveningCourses, getLectureExams, getAssesment } from '../requires'
-
+import * as React from "react";
+import { getLanguages } from "../requires";
 
 import "./start-page.scss";
 import { ProjectCard } from "../../components/project-card/project-card.component";
 
 export class StartPage extends React.PureComponent {
+  state = {
+    languages: []
+  };
 
-	state = {
-		examsType: [],
-	}
+  componentDidMount() {
+    //this.getEveningCourses();
+  }
 
-	componentDidMount() {
-		//this.getEveningCourses();
-	}
+  getLanguages = type => {
+    getLanguages(type).then(data => {
+      this.setState({
+        languages: data
+      });
+    });
+  };
 
-	getEveningCourses = () => {
-		getEveningCourses().then(data => {
-			this.setState({
-				examsType: data
-			})
-		})
-	}
+  render() {
+    const { languages } = this.state;
+    return (
+      <div className="workspace__start">
+        <div className="first-block">
+          <span>Select what type you want to check</span>
+          <div className="button_container">
+            <button
+              className="start_button evening"
+              onClick={() => this.getLanguages("Evening")}
+            >
+              Evening courses
+            </button>
 
-	getLectureExams = () => {
-		getLectureExams().then(data => {
-			this.setState({
-				examsType: data
-			})
-		})
-	}
+            <button
+              className="start_button exam"
+              onClick={this.getLectureExams}
+            >
+              Lectures exam
+            </button>
 
-	getAssesment = () => {
-		getAssesment().then(data => {
-			this.setState({
-				examsType: data
-			})
-		})
-	}
+            <button
+              className="start_button assesment"
+              onClick={this.getAssesment}
+            >
+              Assesment
+            </button>
+          </div>
+        </div>
+        <div className="containerCard">
+          {languages.length ? this.exams : this.noLanguages}
+        </div>
+      </div>
+    );
+  }
 
-	render() {
-		const { examsType } = this.state;
-		return (
-			<div className='workspace__start'>
-				<div className='first-block'>
-					<span>Select what type you want to check</span>
-					<div className='button_container'>
-						<button
-							className='start_button evening'
-							onClick={this.getEveningCourses}>
-								Evening courses
-						</button>
-
-						<button
-							className='start_button exam'
-							onClick={this.getLectureExams}>
-								Lectures exam
-						</button>
-
-						<button
-							className='start_button assesment'
-							onClick={this.getAssesment}>
-								Assesment
-							</button>
-
-					</div>
-				</div>
-				<div className='containerCard'>
-
-					{examsType.length ? this.exams : this.noLanguages}
-				</div>
-			</div>
-		);
-	}
-
-	get noLanguages() {
-		return <span>No language available</span>;
-	}
-	get exams() {
-		return (
-			/* <Link to={'/topic/' + id}> */
-			this.state.examsType.map((e) =>
-				<ProjectCard key={e.id} />
-			)
-			/* </Link> */
-		);
-	}
+  get noLanguages() {
+    return <span>No language available</span>;
+  }
+  get exams() {
+    return (
+      // /* <Link to={'/topic/' + id}> */
+      this.state.languages.map(language => (
+        <ProjectCard
+          key={language.id}
+          name={language.languageType}
+          body={language.description}
+        />
+      ))
+      /* </Link> */
+    );
+  }
 }
