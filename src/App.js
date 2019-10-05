d3ecd02ee102f6cc71544ecd06ccb28720b1128c
Anastasiia_Sokolova <anastasiia_sokolova2@epam.com>
2019-10-05 11:24:01 +0300

    add question page layout

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Header } from "./components/header/header";
 import { StartPage } from "./containers/start-page/start-page.component";
 import { TopicsPage } from "./containers/topic/topics-page.components";
 import { Page } from "./containers/page";
+import { Question } from "./containers/question/questions";
 
 class App extends Component {
   render() {
@@ -12,8 +13,9 @@ class App extends Component {
         <Route component={Header} />
         <Switch>
           <Route exact path='/' component={StartPage} />
-          <Route path='/topics/' component={TopicsPage} />
+          <Route path='/language:langId/topics:examId' component={TopicsPage} />
           <Route path='/topic/:id' component={Page} />
+          <Route path='/question' component={Question} />
         </Switch>
       </React.Fragment>
     );
